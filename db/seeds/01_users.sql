c874314361d0781a084340c144989f4a782a5aac
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-02 16:28:40 -0400

    update

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name,email,phone,password)
+VALUES ('Admin', 'admin@gmail.com','000-000-0000','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+ ('UserA', 'usera@gmail.com','000-000-0001','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('Sae', 'sae@gmail.com','000-000-0002','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('Collin', 'collin@gmail.com','000-000-0003','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('Bolu', 'bolu@gmail.com','000-000-0004','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('UserB', 'userB@gmail.com','000-000-0005','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('UserC', 'userC@gmail.com','000-000-0006','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' ),
+('UserD', 'userD@gmail.com','000-000-0007','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
