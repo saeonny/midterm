3ac16affdf5ec5477586cc58fd05340f18dc5964
Sae <winny9641@gmail.com>
2022-07-02 14:26:56 -0400

    add data schema and sample data

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,51 @@
 -- Drop and recreate Users table (Example)
 -- npm run db:reset // reset db => run on terminal
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+
+  message VARCHAR(255) NOT NULL,
+  message_data_time TIMESTAMP NOT NULL,
+
+)
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  color VARCHAR(255) NOT NULL DEFAULT 'undefined',
+  date_posted TIMESTAMP NOT NULL,
+  available BOOLEAN NOT NULL DEFAULT TRUE,
+  year INTEGER NOT NULL,
+  make VARCHAR(255) NOT NULL,
+  model VARCHAR(255) NOT NULL,
+
+)
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name,email,phone,password)
+VALUES ('Admin', 'admin@gmail.com','000-000-0000','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('UserA', 'usera@gmail.com','000-000-0001','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('Sae', 'sae@gmail.com','000-000-0002','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('Collin', 'collin@gmail.com','000-000-0003','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('Bolu', 'bolu@gmail.com','000-000-0004','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('UserB', 'userB@gmail.com','000-000-0005','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('UserC', 'userC@gmail.com','000-000-0006','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
+VALUES ('UserD', 'userD@gmail.com','000-000-0007','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
