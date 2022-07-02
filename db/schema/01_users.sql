2bbb277b5256afd933da6da48115e8c005c46ccb
Sae <winny9641@gmail.com>
2022-07-02 12:49:11 -0400

    add partial-header

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,5 +1,5 @@
 -- Drop and recreate Users table (Example)
-
+-- npm run db:reset // reset db => run on terminal
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
