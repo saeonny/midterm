6f9ab2b3559b04120c47b23d87a25c7b7124b279
Sae <winny9641@gmail.com>
2022-07-04 13:20:30 -0400

    1

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+
   router.get("/", (req, res) => {
     const user_id = 2; ///////change this part to cookie parser req.session.user_id//////////
     const query =
@@ -14,8 +16,12 @@ module.exports = (db) => {
     db.query(query,[user_id])
       .then(data => {
         const items = data.rows;
-        const vars = {data:items}
+
+        const vars = {data:`
+        <h1>${items[0].id}, ${items[0].title}, $${items[0].price} <button METHOD = POST ACTION = '/delete/${items[0].id}'> delete </button></h1>
+        `}
         res.render("favorites",vars);
+
 
       })
       .catch(err => {
@@ -23,6 +29,26 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
+
   });
+  router.post("/delete/:item", (req, res) =>{
+    const user_id = 2; ///////change this part to cookie parser req.session.user_id//////////
+    const query =
+    `SELECT items.*
+     FROM users
+     JOIN favorites as fav ON users.id = fav.user_id
+     JOIN items ON fav.item_id = items.id
+     WHERE user_id = $1;`
+
+
+  })
   return router;
 };
+
+const dataToHtml = function(data) {
+  let html = ``
+  for (let item of data){
+    html += `${item.id}, ${item.title}`
+
+  }
+}
