1d6e682016d7b86a0c5c10aaec231da5cbbcc010
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-05 13:40:24 -0400

    update favourites

--- routes/favorites.js
+++ routes/favorites.js
@@ -11,7 +11,6 @@ module.exports = (db) => {
     }
 
 
-
     const templateVar = {user_id:null, user_name:null , data:null}
     templateVar.user_id = req.session.user_id;
     templateVar.user_name = req.session.user_name;
@@ -28,14 +27,13 @@ module.exports = (db) => {
      WHERE user_id = $1;`
 
     db.query(query,[user_id])
-      .then(data => {
+    .then(data => {
+        if(data.rows.length === 0) {
+          res.redirect("/");
+        }
         const items = data.rows;
-        templateVar.data = `
-        <h1>${items[0].id}, ${items[0].title}, $${items[0].price} <button METHOD = POST ACTION = '/delete/${items[0].id}'> delete </button></h1>
-        `
+        templateVar.data = dataToHtml(items)
         res.render("favorites",templateVar);
-
-
       })
       .catch(err => {
         res
@@ -45,14 +43,14 @@ module.exports = (db) => {
 
   });
   router.post("/delete/:item", (req, res) =>{
-    const user_id = 2; ///////change this part to cookie parser req.session.user_id//////////
-    const item_id = req.body.item.id; ///// this needs to be changed//////
+    const user_id = req.session.user_id;
+    const item_id = req.body.item.id;
     const query =
-    `SELECT items.*
-     FROM users
-     JOIN favorites as fav ON users.id = fav.user_id
-     JOIN items ON fav.item_id = items.id
-     WHERE user_id = $1;`
+    `DELETE
+     FROM favourites
+     WHERE user_id = $1
+     AND item_id = $2
+     RETURNING *;`
     db.query(query, [user_id, item_id])
       .then(data => {
         res.redirect("/")
@@ -69,12 +67,7 @@ module.exports = (db) => {
 const dataToHtml = function(data) {
   let html = ``
   for (let item of data){
-    html += `<h1>${item.id}, ${item.title}, $${item.price} <button METHOD = POST ACTION = '/delete/${item.id}'> delete </button></h1>`
-    if (!user_id) {
-      html += `<p>Please log in or register to favourite items<p>`
+      html += `<h1>${item.id}, ${item.title}, $${item.price} <button METHOD = POST ACTION = '/delete/${item.id}'> delete </button></h1>`
     }
-    if (user.name ==='Admin' || user.email === 'admin@gmail.com') {
-      html += `<h1>${item.id}, ${item.title}, $${item.price} <button METHOD = POST ACTION = '/delete/${item.id}'> delete </button><button METHOD = POST type = "submit">sold out</button></h1>`
-    }
-  }
+    return html
 }
