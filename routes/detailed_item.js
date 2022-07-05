22d2b8262942db2bb7e8718dfc3bd23614bbee14
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-05 14:14:45 -0400

    basic detailed item html and ejs

---
+++ routes/detailed_item.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+
+  router.get("/:item", (req, res) => {
+    const query =
+    `SELECT *
+     FROM items
+     WHERE item_id = $1;`
+
+    db.query(query,[item_id])
+    .then(data => {
+        const items = data.rows;
+        res.render(items);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  });
+}
