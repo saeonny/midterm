fd67e3b9ffac5131bbc40762abff72597632c8cd
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-06 16:53:18 -0400

    fav updated

--- routes/detailed_item.js
+++ routes/detailed_item.js
@@ -1,25 +1,95 @@
 const express = require('express');
 const favorites = require('./favorites');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
 
   router.get("/:item", (req, res) => {
-    const templateVar = {user_id :null, user_name:null, data:null}
-    if(req.session.user_id){
+    const templateVar = { user_id: null, user_name: null, data: null };
+    if (req.session.user_id) {
       templateVar.user_id = req.session.user_id;
       templateVar.user_name = req.session.user_name;
+      const item_id = req.params.item;
+      const query = `
+      SELECT *
+      FROM favorites
+      WHERE user_id = $1
+      AND item_id = $2
+      `;
+      db.query(query, [req.session.user_id, item_id])
+        .then((result) => {
+          if (result.rows.length === 0) {
+            const item = result.rows[0];
+            const html = `
+          <div class = "detailed_item_card">
+          <img class="itemphoto" src=${item.thumbnail_photo_url}>
+          <form>
+          <div><strong>Item #:</strong> ${item.id}</div>
+          <div><strong>Car Title:</strong> ${item.title}</div>
+          <div><strong>Description:</strong> ${item.description}</div>
+          <div><strong>Price:</strong> $${item.price}</div>
+          <div><strong>Colour:</strong> ${item.color}</div>
+          <div><strong>Date posted:</strong> ${item.date_posted}</div>
+          <div><strong>Item available?</strong> ${item.available}</div>
+          <div><strong>Year:</strong> ${item.year}</div>
+          <div><strong>Make:</strong> ${item.make}</div>
+          <div><strong>Model:</strong> ${item.model}</div>
+          </form>
+          <div class = buttons>
+          <form method = "POST" action = "/home/messages/${item.id}">
+          <button class="contactbutton"> contact </button>
+          </form>
+          <form method = "POST" action = "/home/favourites/${item.id}">
+          <button class="favbutton"> add to favourites </button>
+          </form>
+          </div>
+          </div>
+          `;
+            templateVar.data += html;
+            return res.render("detailed_item", templateVar);
+          }
+          if (result.rows.length !== 0) {
+            const item = result.rows[0];
+            const html = `
+          <div class = "detailed_item_card">
+          <img class="itemphoto" src=${item.thumbnail_photo_url}>
+          <form>
+          <div><strong>Item #:</strong> ${item.id}</div>
+          <div><strong>Car Title:</strong> ${item.title}</div>
+          <div><strong>Description:</strong> ${item.description}</div>
+          <div><strong>Price:</strong> $${item.price}</div>
+          <div><strong>Colour:</strong> ${item.color}</div>
+          <div><strong>Date posted:</strong> ${item.date_posted}</div>
+          <div><strong>Item available?</strong> ${item.available}</div>
+          <div><strong>Year:</strong> ${item.year}</div>
+          <div><strong>Make:</strong> ${item.make}</div>
+          <div><strong>Model:</strong> ${item.model}</div>
+          </form>
+          <div class = buttons>
+          <form method = "POST" action = "/home/messages/${item.id}">
+          <button class="contactbutton"> contact </button>
+          </form>
+          <form method = "POST" action = "/home/favourites/${item.id}">
+          <button class="favbutton"> remove from favourites </button>
+          </form>
+          </div>
+          </div>
+          `;
+            templateVar.data.data += html;
+          }
+        });
     }
+
     const item_id = req.params.item;
     const query = `
     SELECT *
     FROM items
     WHERE id = $1
-    `
-    db.query(query,[item_id])
-    .then((result)=> {
-      const item = result.rows[0];
-      const html = `
+    `;
+    db.query(query, [item_id])
+      .then((result) => {
+        const item = result.rows[0];
+        const html = `
       <div class = "detailed_item_card">
       <img class="itemphoto" src=${item.thumbnail_photo_url}>
       <form>
@@ -43,92 +113,63 @@ module.exports = (db) => {
       </form>
       </div>
       </div>
-      `
-      templateVar.data += html;
-      return res.render("detailed_item",templateVar)
-    })
+      `;
+        templateVar.data += html;
+        return res.render("detailed_item", templateVar);
+      });
 
 
-    if (favorites.user_id === item_id) {
-      const item = result.rows[0];
-      const html = `
-      <div class = "detailed_item_card">
-      <img class="itemphoto" src=${item.thumbnail_photo_url}>
-      <form>
-      <div><strong>Item #:</strong> ${item.id}</div>
-      <div><strong>Car Title:</strong> ${item.title}</div>
-      <div><strong>Description:</strong> ${item.description}</div>
-      <div><strong>Price:</strong> $${item.price}</div>
-      <div><strong>Colour:</strong> ${item.color}</div>
-      <div><strong>Date posted:</strong> ${item.date_posted}</div>
-      <div><strong>Item available?</strong> ${item.available}</div>
-      <div><strong>Year:</strong> ${item.year}</div>
-      <div><strong>Make:</strong> ${item.make}</div>
-      <div><strong>Model:</strong> ${item.model}</div>
-      </form>
-      <div class = buttons>
-      <form method = "POST" action = "/home/messages/${item.id}">
-      <button class="contactbutton"> contact </button>
-      </form>
-      <form method = "POST" action = "/home/favourites/${item.id}">
-      <button class="favbutton"> remove from favourites </button>
-      </form>
-      </div>
-      </div>
-      `
-      templateVar.data.data += html;
-    }
-  //post for contact
-  //post for adding to favorites // if not logined user => redirect to login
+    //post for contact
+    //post for adding to favorites // if not logined user => redirect to login
 
-  // router.post("/home/favourites/:item", (req, res) =>{
-  //   if(!req.session.user_id|| req.session.user_id === 1){
-  //     return res.redirect("/home/login")
-  //   }
+    router.post("/home/favourites/:item", (req, res) => {
+      if (!req.session.user_id || req.session.user_id === 1) {
+        return res.redirect("/home/login");
+      }
 
-  //   const user_id = req.session.user_id;
-  //   const item_id = req.params.item;
-  //   const query =
-  //   `INSERT
-  //    FROM favorites
-  //    WHERE user_id = $1
-  //    AND item_id = $2
-  //    RETURNING *;`
-  //   db.query(query, [user_id, item_id])
-  //     .then(data => {
-  //       res.redirect("/home/favorites")
-  //   })
-  //     .catch(err => {
-  //       res
-  //       .status(500)
-  //       .json({error: err.message})
-  //   });
-  // })
+      const user_id = req.session.user_id;
+      const item_id = req.params.item;
+      const query =
+        `INSERT
+     FROM favorites
+     WHERE user_id = $1
+     AND item_id = $2
+     RETURNING *;`;
+      db.query(query, [user_id, item_id])
+        .then(data => {
+          res.redirect("/home/item/:item");
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+      })
 
-  // router.post("/home/messages/:item", (req, res) =>{
-  //   if(!req.session.user_id|| req.session.user_id === 1){
-  //     return res.redirect("/home/login")
-  //   }
+      // router.post("/home/messages/:item", (req, res) =>{
+      //   if(!req.session.user_id|| req.session.user_id === 1){
+      //     return res.redirect("/home/login")
+      //   }
 
-  //   const user_id = req.session.user_id;
-  //   const item_id = req.params.item;
-  //   const query =
-  //   `INSERT
-  //    FROM messages
-  //    WHERE user_id = $1
-  //    AND item_id = $2
-  //    RETURNING *;`
-  //   db.query(query, [user_id, item_id])
-  //     .then(data => {
-  //       res.redirect("/home/messages/:item")
-  //   })
-  //     .catch(err => {
-  //       res
-  //       .status(500)
-  //       .json({error: err.message})
-  //   });
-  // })
+      //   const user_id = req.session.user_id;
+      //   const item_id = req.params.item;
+      //   const query =
+      //   `INSERT
+      //    FROM messages
+      //    WHERE user_id = $1
+      //    AND item_id = $2
+      //    RETURNING *;`
+      //   db.query(query, [user_id, item_id])
+      //     .then(data => {
+      //       res.redirect("/home/messages/:item")
+      //   })
+      //     .catch(err => {
+      //       res
+      //       .status(500)
+      //       .json({error: err.message})
+      //   });
+      // })
 
-  });
-  return router;
-}
+    });
+    return router;
+  };
