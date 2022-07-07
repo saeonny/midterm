ada8304af5f4551ae5c3a865fb7316f79d100e06
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-07 12:08:43 -0400

    update messages send css

--- routes/send_message.js
+++ routes/send_message.js
@@ -184,7 +184,7 @@ const messagesToHtml = function(messages, item_id, user_id,for_user_id) {
     if(message.sender_id === user_id) {
       html += `
       <div class = sender>
-        <p>me:<br>
+        <p><strong>me:</strong><br>
            ${message.message}</p>
       </div>
       `
@@ -192,7 +192,7 @@ const messagesToHtml = function(messages, item_id, user_id,for_user_id) {
     if(message.sender_id === 1) {
       html += `
       <div class = "receiver">
-        <p>admin:<br>
+        <p><strong>admin:</strong><br>
         ${message.message}</p>
         </div>
 
@@ -204,8 +204,10 @@ const messagesToHtml = function(messages, item_id, user_id,for_user_id) {
   </div>
   <form class="message-container" action="/home/send/messages/${item_id}/for/${for_user_id}" method="POST">
 
-        <textarea name="text" class="message-text"></textarea>
+        <textarea name="text" class="message-text" placeholder="Send message here"></textarea>
+        <div>
         <button class="message-button" type="submit">Send</button>
+        </div>
   </form>
 
   `
