11a4b28414d06dfa889c538664c3753b6f61e477
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-07 18:58:20 -0400

    update messages db

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -11,10 +11,8 @@ INSERT INTO messages (sender_id,receiver_id, item_id, message,message_data_time)
  (2,1,3,'hello i want item 3 :)','2022-06-27 07:33:04' ),
  (1,2,3,'sure it would be $500000','2022-06-27 07:42:06' ),
 
-
  (2,1,4,'hello i want item 4 :)','2022-06-29 11:23:54' ),
  (1,2,4,'no :( not available anymore!','2022-06-29 11:26:54' ),
-
 
  (3,1,1,'hello i want item 1 :)','2022-06-29 20:23:32' ),
  (1,3,1,'you are on waiting list then','2022-06-29 20:23:34' ),
@@ -29,15 +27,11 @@ INSERT INTO messages (sender_id,receiver_id, item_id, message,message_data_time)
  (1,5,3,'great, it is available, we will contact you to provide more info!','2022-05-25 10:08:54' ),
 
 
- (2,1,3,'hello i want item 3 :)','2022-05-27 07:33:04' ),
- (1,2,3,'sorry, no longer available','2022-05-28 10:35:54' ),
-
-
  (2,1,4,'hello i want item 4 :)','2022-06-24 23:23:34' ),
  (1,2,4,'okay, it will cost $50000','2022-06-26 16:35:54' ),
 
- (3,1,1,'hello i want item 1 :)','2022-06-21 07:33:51' ),
- (1,3,1,'great, it is available, it will be $20000','2022-06-21 08:08:54' ),
+ (3,1,6,'hello i want item 1 :)','2022-06-21 07:33:51' ),
+ (1,3,6,'great, it is available, it will be $20000','2022-06-21 08:08:54' ),
 
  (5,1,6,'hello i want item 6 :)','2022-06-21 07:33:51' ),
  (1,5,6,'great! that will be $80000','2022-06-21 08:08:54' );
